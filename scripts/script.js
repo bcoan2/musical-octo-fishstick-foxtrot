a0b0e124f59a9a6290ae13ecde6bfd3c10c62c30
bcoan2 <122556056+bcoan2@users.noreply.github.com>
2023-03-12 13:13:45 -0500

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,30 +1,51 @@
-$(document).ready(function() {
+const data = [{
+    id: 1,
+    show: 'The Last of us',
+    place: 'HBO Max',
+    status: 'Caught up',
+  }, {
+    id: 2,
+    show: 'Friends',
+    place: 'HBO Max',
+    status: 'Watching',
+  }, {
+    id: 3,
+    show: 'Outer Banks',
+    place: 'Netflix',
+    status: 'Watching',
+  }, {
+    id: 4,
+    show: 'Tulsa King',
+    place: 'Prime Video',
+    status: 'Caught up',
+  }];
   
-    //   here is an array of objects. Each array item is an object.
-    const myDataArrayOfObjects = [
-       {
-       show: "last of us",
-         status: "not caught up yet but close"
-        },
-       {
-        show: "MH 370: The Plane That Disappeared",
-         venue: "Netflix, yo!"
-         },
-      {
-        show: "Are You The One, Season 9",
-        notes: "this show is so bad. so good. but so bad. but good. "
-         }
-      ];
-    
-      
-     $.each(myDataArrayOfObjects, (index, item) => {
-       
-       $('#thisP').append("<li>" + item.show + "</li>");
-       
-     
-       
-     })
-      
-    $('#thisP').append("this " + myDataArrayOfObjects[0].show + ", " + "status = " + myDataArrayOfObjects[0].status); 
-    
+  // Reference <tbody>
+  const tB = document.querySelector("table").tBodies[0];
+  
+  /**
+   * @desc - Generates table rows from data. A <table> must be in DOM.
+   * @param {object<DOM>}   tbody   - Reference to a <table> or <tbody>
+   * @param {array<object>} array   - An array of objects
+   * @param {number<rest>}  headers - One or more numbers representing the
+   *        index numbers of the first object of the array's keys. If 
+   *        undefined it @default to all of the keys.
+   */
+  function createRows(tbody, array, ...headers) {
+
+    const keys = Object.keys(array[0]); 
+    let hdrs;
+
+    if ([...headers].length < 1) {
+      hdrs = keys;
+    } else {
+      hdrs = keys.filter((_, i) => [...headers].includes(i));
+    }
+
+    array.forEach(obj => {
+      let tr = tbody.insertRow();
+      hdrs.forEach(key => tr.insertCell().textContent = obj[key]);
     });
+  }
+  
+  createRows(tB, data, 1, 2, 3);
